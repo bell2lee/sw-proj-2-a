6f971aae2ac5db3cd2dd70554e0bb811df004eac
jang han yang <qpwisu@gmail.com>
2017-09-07 09:36:57 +0900

    한영

---
+++ 20171696-장한영-assignment.py
@@ -0,0 +1,14 @@
+while True:
+    s= int(input())
+    m=0
+    for i in range(1,s+1):
+        a = 0
+        m= m+i
+    if s ==0:
+        m=1
+
+
+    print(m)
+    if s == -1:
+            break
+
