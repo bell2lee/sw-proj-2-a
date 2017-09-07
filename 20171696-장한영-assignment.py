39e0738834d2d8c54e83d57aeda62dce18d7bc75
qpwisu <qpwisu@gmail.com>
2017-09-07 09:41:59 +0900

    Update 20171696-장한영-assignment.py

--- 20171696-장한영-assignment.py
+++ 20171696-장한영-assignment.py
@@ -1,14 +1,9 @@
-while True:
-    s= int(input())
-    m=0
-    for i in range(1,s+1):
-        a = 0
-        m= m+i
-    if s ==0:
-        m=1
+def Facto(num) :
+    if num > 1 :
+        return (num * Facto(num-1))
+    else :
+        return 1
 
 
-    print(m)
-    if s == -1:
-            break
+print(Facto(20)) 
 
