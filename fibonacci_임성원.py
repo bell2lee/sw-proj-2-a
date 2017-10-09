54a40f886eabe2154ea35bc232c3386570ba5b69
SungWon Lim <sungwontoto@kookmin.ac.kr>
2017-10-10 01:20:50 +0900

    시간 비교 추가

--- fibonacci_임성원.py
+++ fibonacci_임성원.py
@@ -1,18 +1,33 @@
-def fibo(n):
-    if n == 0:
+import time
+
+#재귀함수를 이용한 방법
+def fibo(num):
+    if num ==1 or num ==2:
+        return 1
+    else:
+        return fibo(num-1) + fibo(num-2)
+
+
+#점화식을 이용한 방법
+def iterfibo(num):
+    if num == 0:
         return 0
-    elif n == 1:
+    elif num == 1:
         return 1
     else:
         a,b = 1,1
-        for i in range(n-1):
+        for i in range(num-1):
             a,b = b,a+b
         return a
-k = 1
-while (k>=0):
-    k = int(input("Enter a Number: "))
-    if k < 0:
-        print("양수를 입력해 주세요.")
-        break
-    else:
-        print(fibo(k))
+
+nbr = 1
+while (nbr>0):
+    nbr = int(input("Enter a number: "))
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
