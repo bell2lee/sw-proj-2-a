7886db83e4c89e31b1e4f3b25f09fc11ddf59de4
hahooon <plmokn1007@kookmin.ac.kr>
2017-11-09 09:13:47 +0900

    calculator assignment1

---
+++ 20171697_assignment.py
@@ -0,0 +1,122 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QLineEdit, QToolButton
+from PyQt5.QtWidgets import QSizePolicy
+from PyQt5.QtWidgets import QLayout, QGridLayout
+import decimal
+
+
+class Button(QToolButton):
+
+    def __init__(self, text, callback):
+        super().__init__()
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.setText(text)
+        self.clicked.connect(callback)
+
+    def sizeHint(self):
+        size = super(Button, self).sizeHint()
+        size.setHeight(size.height() + 20)
+        size.setWidth(max(size.width(), size.height()))
+        return size
+
+class Calculator(QWidget):
+
+    def buttonClicked(self):
+        button = self.sender()
+        key = button.text()
+        if key == '=':
+            #부동소수 처리하는 곳.
+            num = eval(self.display.text())
+            integer_part = int(num)
+            decimal_part = str(num - integer_part)
+            if float(decimal_part) != 0:
+                for i in range(len(decimal_part)-2):
+                    if decimal_part[i] == decimal_part[i+1] == decimal_part[i+2]:
+                        conclusion_num = integer_part + round(float(decimal_part[0:i+1]), i-2)
+                        result = str(conclusion_num)
+                        break
+            else:
+                result = str(integer_part)
+            self.display.setText(result)
+        elif key == 'C':
+            self.display.setText("")
+        else:
+            self.display.setText(self.display.text() + key)
+
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        # Display Window
+        self.display = QLineEdit()
+        self.display.setReadOnly(True)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setMaxLength(15)
+        
+        # Digit Buttons
+        self.digitButton = [x for x in range(0, 10)]
+        for i in range(10):
+            self.digitButton[i] = Button('%d' %i, self.buttonClicked)
+
+        # . and = Buttons
+        self.decButton = Button('.', self.buttonClicked)
+        self.eqButton = Button('=', self.buttonClicked)
+
+        # Operator Buttons
+        self.mulButton = Button('*', self.buttonClicked)
+        self.divButton = Button('/', self.buttonClicked)
+        self.addButton = Button('+', self.buttonClicked)
+        self.subButton = Button('-', self.buttonClicked)
+
+        # Parentheses Buttons
+        self.lparButton = Button('(', self.buttonClicked)
+        self.rparButton = Button(')', self.buttonClicked)
+
+        # Clear Button
+        self.clearButton = Button('C', self.buttonClicked)
+
+        # Layout
+        mainLayout = QGridLayout()
+        mainLayout.setSizeConstraint(QLayout.SetFixedSize)
+
+        mainLayout.addWidget(self.display, 0, 0, 1, 2)
+
+        numLayout = QGridLayout()
+
+        for num in range(9,0,-1):
+            numLayout.addWidget(self.digitButton[num], ((10-num)-1)//3, (num-1)%3)
+
+        numLayout.addWidget(self.digitButton[0],3,0)
+        numLayout.addWidget(self.decButton, 3, 1)
+        numLayout.addWidget(self.eqButton, 3, 2)
+
+        mainLayout.addLayout(numLayout, 1, 0)
+
+        opLayout = QGridLayout()
+
+        opLayout.addWidget(self.mulButton, 0, 0)
+        opLayout.addWidget(self.divButton, 0, 1)
+        opLayout.addWidget(self.addButton, 1, 0)
+        opLayout.addWidget(self.subButton, 1, 1)
+
+        opLayout.addWidget(self.lparButton, 2, 0)
+        opLayout.addWidget(self.rparButton, 2, 1)
+        
+        opLayout.addWidget(self.clearButton, 3, 0)
+        
+        mainLayout.addLayout(opLayout, 1, 1)
+
+        self.setLayout(mainLayout)
+        
+        self.setWindowTitle("My Calculator")
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    sys.exit(app.exec_())
