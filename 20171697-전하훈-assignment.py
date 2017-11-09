9dbf6b5f9316640a1d5d1fa05c5d4b5041215fda
Jong-Hwi Lee <paperlee@kookmin.ac.kr>
2017-11-09 11:03:10 +0900

    delete file

--- 20171697-전하훈-assignment.py
+++
@@ -1,9 +0,0 @@
-while True :
-    num = int(input("숫자를 입력하세요 : "))
-    if num == -1 :
-        print('상수를 입력하십시오!')
-        break
-    s = 1
-    for i in range(1,num+1) :
-        s *= i
-    print(num,'팩터리얼은 ',s,'입니다.')
